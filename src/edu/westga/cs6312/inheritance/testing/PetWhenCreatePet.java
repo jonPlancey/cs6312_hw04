7da8384f215f063b4cb159006b7bf331d909b05a
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-01-31 17:32:10 -0500

    + unit test class and testPetWhenCreatePetRalph added

--- src/edu/westga/cs6312/inheritance/testing/PetWhenCreatePet.java
+++ src/edu/westga/cs6312/inheritance/testing/PetWhenCreatePet.java
@@ -1,5 +1,22 @@
 package edu.westga.cs6312.inheritance.testing;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import edu.westga.cs6312.inheritance.model.Pet;
+
+
 public class PetWhenCreatePet {
+
+	/**
+	 * Test, verify Pet object is correctly created
+	 */		
+	@Test
+	public void testPetWhenCreatePetRalph() {
+		Pet dog = new Pet("Harris", "boo");
+		String expect = "A pet named Ralph that makes the sound woof";
+		String result = dog.toString();
+		assertEquals(expect, result);	
+	}
 
 }
