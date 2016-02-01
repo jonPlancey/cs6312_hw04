47ffe656838cdf7bfac69515e8ae92c49c6bdc7a
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-01-31 19:50:25 -0500

    + PetDogWhenCreatePetDog and tests added

--- src/edu/westga/cs6312/inheritance/testing/PetDogWhenCreatePetDog.java
+++ src/edu/westga/cs6312/inheritance/testing/PetDogWhenCreatePetDog.java
@@ -3,25 +3,18 @@ package edu.westga.cs6312.inheritance.testing;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-import edu.westga.cs6312.inheritance.model.Pet;
+import edu.westga.cs6312.inheritance.model.PetDog;
 
-
-/**
- * Homework 04 Unit Tests
- * @author 	cs6312
- * @author 	jim perry, jr.
- * @version	01.30.2016
- */
-public class PetWhenCreatePet {
+public class PetDogWhenCreatePetDog {
 
 	/**
 	 * Test, verify PetDog object is correctly created
 	 */		
 	@Test
 	public void testPetDogWhenCreatePetRalphWoof() {
-		Pet dog = new Pet("Harris", "boo");		
+		PetDog animal = new PetDog("Harris", "boo");		
 		String expect = "A pet named Harris that makes the sound boo";
-		String result = dog.toString();		
+		String result = animal.toString();		
 		assertEquals(expect, result);	
 	}
 	
@@ -31,9 +24,9 @@ public class PetWhenCreatePet {
 	 */		
 	@Test
 	public void testPetDogWhenCreatePetNalaRoar() {
-		Pet dog = new Pet("Nala", "roar");		
+		PetDog animal = new PetDog("Nala", "roar");		
 		String expect = "A pet named Nala that makes the sound roar";
-		String result = dog.toString();		
+		String result = animal.toString();		
 		assertEquals(expect, result);	
 	}
 	
@@ -43,9 +36,9 @@ public class PetWhenCreatePet {
 	 */		
 	@Test
 	public void testPetDogWhenCreatePetVioletArf() {
-		Pet dog = new Pet("Violet", "arf");		
+		PetDog animal = new PetDog("Violet", "arf");		
 		String expect = "A pet named Violet that makes the sound arf";
-		String result = dog.toString();		
+		String result = animal.toString();		
 		assertEquals(expect, result);	
 	}
 	
@@ -55,9 +48,9 @@ public class PetWhenCreatePet {
 	 */		
 	@Test
 	public void testPetDogWhenCreatePetBoltOwloo() {
-		Pet dog = new Pet("Bolt", "owloo");		
+		PetDog animal = new PetDog("Bolt", "owloo");		
 		String expect = "A pet named Bolt that makes the sound owloo";
-		String result = dog.toString();		
+		String result = animal.toString();		
 		assertEquals(expect, result);	
 	}	
 
