db95f31945ba54d550bb371abe01f2909e212f48
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-01-31 19:29:46 -0500

    + petDog, constructors and toString method added

--- src/edu/westga/cs6312/inheritance/model/PetDog.java
+++ src/edu/westga/cs6312/inheritance/model/PetDog.java
@@ -9,15 +9,56 @@ package edu.westga.cs6312.inheritance.model;
 public class PetDog extends Pet{
 	private int soundLevel;
 	
+
 	/** 
 	 * Pet constructor 
+	 * @param petName accepts pet's name
+	 * @param petSound accepts the sound pet makes
+	 * @precondition if (this.sound == null)
+	 * @precondition if (this.name == null) 
+	 */
+	public PetDog(String petName, String petSound) {
+		super(petName, petSound);
+		this.soundLevel = 0;			
+	}
+	
+	/** 
+	 * Pet constructor 
+	 * @param petName accepts pet's name
+	 * @param petSound accepts the sound pet makes
+	 * @param soundLevel accepts the pet's volume level 
+	 * @precondition if (this.sound == null)
+	 * @precondition if (this.name == null) 
+	 * @precondition if (this.soundLevel == 0)    
 	 */
 	public PetDog(String petName, String petSound, int level) {
 		super(petName, petSound);
-		
-		if (this.soundLevel == 0) {
-			this.soundLevel = 5;	
+		if (level >= 0) {
+			this.soundLevel = level;
 		}
-		this.soundLevel = level;
 	}
+	
+	/** 
+	 * Method returns pet's name
+	 * @return pet's name  
+	 */
+	public String getPetName() {					
+		return super.getPetName();
+	}	
+	
+	/** 
+	 * Method returns pet's sound
+	 * @return pet's sound  
+	 */
+	public String getPetSound() {					
+		return super.getPetSound();
+	}
+	
+	/** 
+	 * Method returns string object describing pet
+	 * @return string object, description of pet name and sound  
+	 */
+	public String toString() {		
+		return super.toString() + " at level " + this.soundLevel;
+	}	
 }
