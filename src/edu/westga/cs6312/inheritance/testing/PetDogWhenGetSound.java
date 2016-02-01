9a8ce99874f7abe0f0281868abf0cb9fe5a10d8f
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-01-31 20:10:35 -0500

    + PetDogWhenGetSound, test both 2 and 3 param constructor, added

--- src/edu/westga/cs6312/inheritance/testing/PetDogWhenGetSound.java
+++ src/edu/westga/cs6312/inheritance/testing/PetDogWhenGetSound.java
@@ -8,52 +8,52 @@ import edu.westga.cs6312.inheritance.model.PetDog;
 public class PetDogWhenGetSound {
 
 	/**
-	 * Test, make sure getPetDogSoun returns
+	 * Test, make sure getPetDogSound returns
 	 * the correct sound of the created pet
 	 */	
 	@Test
 	public void testPetDogWhenGetSoundBoo() {
 		PetDog pet = new PetDog("Harris", "boo");		
-		String expect = "boo";
+		int expect = 0;
 		String result = pet.getPetDogSound();		
 		assertEquals(expect, result);	
 	}
 	
 
 	/**
-	 * Test, make sure getPetDogSoun returns
+	 * Test, make sure getPetDogSound returns
 	 * the correct sound of the created pet
 	 */		
 	@Test
 	public void testPetDogWhenGetSoundRoar() {
 		PetDog pet = new PetDog("Nala", "roar");		
-		String expect = "roar";
+		int expect = 0;
 		String result = pet.getPetDogSound();		
 		assertEquals(expect, result);		
 	}
 	
 
 	/**
-	 * Test, make sure getPetDogSoun returns
+	 * Test, make sure getPetDogSound returns
 	 * the correct sound of the created pet
 	 */		
 	@Test
 	public void testPetDogWhenGetSoundArf() {
-		PetDog pet = new PetDog("Violet", "arf");		
-		String expect = "arf";
+		PetDog pet = new PetDog("Violet", "arf", 77);		
+		int expect = 77;
 		String result = pet.getPetDogSound();		
 		assertEquals(expect, result);	
 	}
 	
 
 	/**
-	 * Test, make sure getPetDogSoun returns
+	 * Test, make sure getPetDogSound returns
 	 * the correct sound of the created pet
 	 */		
 	@Test
 	public void testPetDogWhenGetSoundOwloo() {
-		PetDog pet = new PetDog("Bolt", "owloo");		
-		String expect = "owloo";
+		PetDog pet = new PetDog("Bolt", "owloo", 11);		
+		int expect = 11;
 		String result = pet.getPetDogSound();		
 		assertEquals(expect, result);	
 	}	
